43aab60e1057ff0a432c566f01afeec0df1915a5
Krisztián Gesztiványi <qxy@qxy.hu>
2020-02-28 07:05:51 +0100

    initial commit

---
+++ Mod2Text/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Abdul
+ * Date: 2020.02.28.
+ * Time: 7:00
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Mod2Text
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
